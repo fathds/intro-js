6212fd21ca82bfbd069de606f4bb91f5d742e1ba
Fátima Hernández <fatimaitvoax@gmail.com>
2022-03-09 20:01:59 -0600

    confirmar

---
+++ Actividad 3/condicionales.js
@@ -0,0 +1,24 @@
+var condicion = true;
+
+//Programa que pida nombre y edad
+// Si es mayor de edad puede entrar a la discoteca
+// Si se llama Mario y es mayor de edad entra a VIP
+// 
+
+/*var nombre = "mario";
+var edad = 12;
+if(nombre === "mario" && edad >= 18){
+    console.log("Puedes entrar a la discoteca a la sala VIP");
+} else if(edad >= 18){
+    console.log("Puedes entrar a la discoteca");
+}
+else{
+    console.log("No puedes entrar a la discoteca, no tienes edad");
+}*/
+
+
+var comida;
+
+switch(comida){
+    case
+}
